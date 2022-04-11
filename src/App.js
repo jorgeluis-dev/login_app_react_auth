b6ba9b3c54117e83c1ac0b2901657ca6a0f05f91
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-04-10 23:34:56 -0300

    login app simples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Logo from './logo.svg';
+
+import "./App.css";
 
 function App() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const url = "https://github.com/jorgeluis-dev"
+  // User Login info
+  const database = [
+    {
+      email: "jorgeluis.geek@gmail.com",
+      password: "react"
+    }
+  ];
+
+  const errors = {
+    emailError: "escreva um email valido",
+    pass: "senha incorreta"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { emailError, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.email === emailError.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "EmailError", message: errors.emailError });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (emailError) =>
+    emailError === errorMessages.emailError && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input alt="Por favor escreva o seu endereço de email" placeholder="EMAIL" type="text" name="emailError" required />
+          {renderErrorMessage("emailError")}
+        </div>
+        <div className="input-container">
+          <input alt="Por favor escreva sua Senha para acessar o Sistema" placeholder="SENHA" type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+        <div className="forgotpassword"><a href={url} target="_blank">Esqueceu sua Senha?</a></div>
+      </form>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="login-form">
+        <img src={Logo} alt="logo" />
+        <div className="title"></div>
+        {isSubmitted ? <div>Login realizado com sucesso</div> : renderForm}
+      </div>
     </div>
   );
 }
